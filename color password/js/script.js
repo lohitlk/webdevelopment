f286de188c46df40abeaff04622798782680ca8f
lohitlk <rockinglohitkumar@gmail.com>
2020-07-25 09:22:35 +0530

    added pass

---
+++ color password/js/script.js
@@ -0,0 +1,103 @@
+const butt1=document.getElementById("butt1");
+const butt2=document.getElementById("butt2");
+const butt3=document.getElementById("butt3");
+const butt4=document.getElementById("butt4");
+const butt5=document.getElementById("butt5");
+const butt6=document.getElementById("butt6");
+const butt7=document.getElementById("butt7");
+const butt8=document.getElementById("butt8");
+const butt9=document.getElementById("butt9");
+const butt10=document.getElementById("butt10");
+const butt11=document.getElementById("butt11");
+const butt12=document.getElementById("butt12");
+const textbox=document.getElementById("textbox");
+var a1="1aS!";
+var a2="2sD@";
+var a3="3dF#";
+var a4="4fG$";
+var a5="5gH%";
+var a6="6hJ^";
+var a7="7jK*";
+var a8="8kL?";
+var a9="9lZ!";
+var a10="0zX@";
+var a11="11xC#";
+var a12="12cB$";
+for(var i=0; i<=4; i++){
+butt1.addEventListener('click',function pass(){
+			textbox.setAttribute("value",a1);
+		});
+butt2.addEventListener('click',function pass(){
+			textbox.setAttribute("value",a2);
+		});
+butt3.addEventListener('click',function pass(){
+			textbox.setAttribute("value",a3);
+		});
+butt4.addEventListener('click',function pass(){
+			textbox.setAttribute("value",a4);
+		});
+butt5.addEventListener('click',function pass(){
+			textbox.setAttribute("value",a5);
+		});
+butt6.addEventListener('click',function pass(){
+			textbox.setAttribute("value",a6);
+		});
+butt7.addEventListener('click',function pass(){
+			textbox.setAttribute("value",a7);
+		});
+butt8.addEventListener('click',function pass(){
+			textbox.setAttribute("value",a8);
+		});
+butt9.addEventListener('click',function pass(){
+			textbox.setAttribute("value",a9);
+		});
+butt10.addEventListener('click',function pass(){
+			textbox.setAttribute("value",a10);
+		});
+butt11.addEventListener('click',function pass(){
+			textbox.setAttribute("value",a11);
+		});
+butt12.addEventListener('click',function pass(){
+			textbox.setAttribute("value",a12);
+		});
+}
+
+/*for(var i=0; i<4; i++){
+	function pass1(){
+			textbox.setAttribute("value",a1);
+		}
+	function pass2(){
+			textbox.setAttribute("value",a2);
+		}
+	function pass3(){
+			textbox.setAttribute("value",a3);
+		}
+	function pass4(){
+			textbox.setAttribute("value",a4);
+		}
+	function pass5(){
+			textbox.setAttribute("value",a5);
+		}
+	function pass6(){
+			textbox.setAttribute("value",a6);
+		}
+	function pass7(){
+			textbox.setAttribute("value",a7);
+		}
+	function pass8(){
+			textbox.setAttribute("value",a8);
+		}
+	function pass9(){
+			textbox.setAttribute("value",a9);
+		}
+	function pass10(){
+			textbox.setAttribute("value",a10);
+		}
+	function pass11(){
+			textbox.setAttribute("value",a11);
+		}
+	function pass12(){
+			textbox.setAttribute("value",a12);
+		}
+}
+*/
